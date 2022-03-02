a4e901ffe255075a6f5c69c7a8c2063a6a8df0cc
williamwhang <whang.william@gmail.com>
2022-03-02 10:07:57 -0300

    Criado componente Header e List

--- App.js
+++ App.js
@@ -1,10 +1,69 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+import Header from './src/Header';
+import List from './src/List';
 
 export default function App() {
+  const [feed, setFeed] = useState([
+    {
+      id: '1',
+      nome: 'Lucas Silva',
+      descricao: 'Mais um dia de muitos bugs :)',
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',
+      likeada: true,
+      likers: 1
+    },
+    {
+      id: '2',
+      nome: 'Matheus',
+      descricao: 'Isso sim é ser raiz!!!!!',
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png',
+      likeada: false,
+      likers: 0
+    },
+    {
+      id: '3',
+      nome: 'Jose Augusto',
+      descricao: 'Bora trabalhar, hoje estou começando em um projeto novo aqui no sujeito, desde o backend ao frontend',
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',
+      likeada: false,
+      likers: 3
+    },
+    {
+      id: '4',
+      nome: 'Gustavo Henrique',
+      descricao: 'Isso sim que é TI!',
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png',
+      likeada: false,
+      likers: 1
+    },
+    {
+      id: '5',
+      nome: 'Guilherme',
+      descricao: 'Boa tarde galera do insta...',
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+      likeada: false,
+      likers: 32
+    }
+  ])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> Fake Instagram </Text>
+      <Header />
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        data={feed}
+        renderItem={({ item }) => <List data={item} />}
+      />
+
     </View>
   )
 }
@@ -12,13 +71,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  text: {
-    color: 'white',
-    fontSize: 35,
-    fontWeight: 'bold',
-    backgroundColor: 'red',
-    textAlign: 'center',
+    flex: 1,
   },
 });
---
+++ src/List/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function List() {
+    return (
+        <View >
+
+            <Text> Lista FEED</Text>
+        </View>
+    )
+}
